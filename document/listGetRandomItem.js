9b1594ca8178108f4df97fb435fd60abe73a1f6d
RealMaybe <realmaybe0429@qq.com>
2023-07-11 16:47:35 +0800

    listGetRandomItem

---
+++ document/listGetRandomItem.js
@@ -0,0 +1,8 @@
+function listGetRandomItem(list, remove) {
+    let x = Math.floor(Math.random() * list.length);
+    if (remove) {
+        return list.splice(x, 1)[0];
+    } else {
+        return list[x];
+    }
+};
