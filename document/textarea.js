00e1967ac1e72492185d0b4d589588d4c78ef304
RealMaybe <realmaybe0429@qq.com>
2023-07-14 16:14:13 +0800

    text

--- document/textarea.js
+++ document/textarea.js
@@ -25,12 +25,21 @@
     };
 })(jQuery);
 
-
 // 调用实例
 $(".textarea").autoTextarea({
     maxHeight: 220, //文本框是否自动撑高，默认：null，不自动撑高；如果自动撑高必须输入数值，该值作为文本框自动撑高的最大高度
 });
 
+/*
+ * 根据Value格式化为带有换行、空格格式的HTML代码
+ * @param strValue {String} 需要转换的值
+ * @return  {String}转换后的HTML代码
+ * @example  
+ * getFormatCode("测\r\n\s试")  =>  “测<br/> 试”
+ */
+function getFormatCode(strValue) {
+    return strValue.replace(/\r\n/g, "<br/>").replace(/\n/g, "<br/>").replace(/\s/g, " ");
+};
 
 // 调用实例
 $("textarea").attr("maxlength", 10000).on("input keyup", function() {
