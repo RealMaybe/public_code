d304267f330cc6c6a7b1f3e02cb0671619ee4ab2
RealMaybe <realmaybe0429@qq.com>
2023-08-28 13:00:19 +0800

    random

--- document/random.js
+++ document/random.js
@@ -1,5 +1,8 @@
-/* 范围随机数生成 */
-function getRandom(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-};
-/* 调用时输入 min 和 max 的值，可以获取到这个范围内所有的数字 */
+/* 
+ * 范围随机数生成
+ * 调用时输入 min 和 max 的值，可以获取到这个范围内所有的数字
+ */
+function getRandom(min, max) { return Math.floor(Math.random() * (max - min + 1) + min); };
+
+/* 数字如果小于零则一直显示0 */
+function numZero(n) { return n > 0 ? n : n <= 0 ? 0 : void 0 }
