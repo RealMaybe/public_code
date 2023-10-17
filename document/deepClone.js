76d2328ae1c97b95d44f0fafe278a061a47f932a
RealMaybe <realmaybe0429@qq.com>
2023-10-17 14:00:33 +0800

    deepClone

---
+++ document/deepClone.js
@@ -0,0 +1,14 @@
+/**
+ * deep clone
+ * 深度克隆
+ * @param { object } obj 
+ */
+function deepClone(obj) {
+    return new Promise(resolve => {
+        const { port1, port2 } = new MessageChannel();
+        port1.postMessage(obj);
+        port2.onmessage = msg => {
+            resolve(msg.data)
+        }
+    })
+}
