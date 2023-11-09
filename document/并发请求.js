85278f2e147a0dae2e94bbd852b6bc2cdd43946a
RealMaybe <realmaybe0429@qq.com>
2023-11-09 23:33:52 +0800

    并发请求

---
+++ document/并发请求.js
@@ -0,0 +1,28 @@
+/**
+ * 并发请求
+ * @param { string[] } urls 待请求的 url 数组
+ * @param { number } maxNum 最大并发数
+ */
+function consurRequest(urls, maxNum) {
+    return new Promise(resolve => {
+        if (urls.length === 0) { resolve([]) }
+        let index = 0, // 下一次请求对应的url地址下标
+            count = 0; // 请求完成的数量
+        const result = [];
+
+        async function request() {
+            const i = index;
+            const url = urls[index++];
+            try {
+                const resp = await fetch(url)
+            } catch (err) {
+                result[i] = err
+            } finally {
+                count++;
+                if (count === urls.length) { resolve(result) }
+                if (index < urls.length) { request() }
+            }
+        }
+        for (let i = 0; i < Math.min(urls.length, maxNum); i++) { request() }
+    })
+}
