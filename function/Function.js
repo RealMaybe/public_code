79742ccdfd1ed66b580c627525b41ed6d4a42c30
RealMaybe <realmaybe0429@qq.com>
2024-01-03 01:18:34 +0800

    agreement

--- function/Function.js
+++ function/Function.js
@@ -35,3 +35,32 @@ const formatSeconds = s => new Date(s * 1000).toISOString().substring(11, 19);
 
 // 范围随机数生成
 const getRandom = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+
+// 复制文本到剪贴板
+const copyClipboard = text => navigator.clipboard.writeText(text);
+// 兼容性存在问题
+
+// 获取某个日期位于当年的第几天
+const dayOfYear = date => Math.floor((date - new Date(date.getFullYear(), 0, 0)) / 1000 / 60 / 60 / 24);
+
+// 将RGB颜色灰度化（基于光感加权平均）
+const ColorGray = (r, g, b) => 0.2126 * r + 0.7152 * g + 0.0722 * b;
+
+// 解析URL中的参数
+const parseQuery = url => {
+    q = {};
+    url.replace(/([^?&=]+)=([^&]+)/g, (_, k, v) => (q[k] = v));
+    return q;
+};
+
+// 筛选对象属性
+const pick = (obj, ...props) => Object.fromEntries(Object.entries(obj).filter(([k]) => props.includes(k)));
+
+// 随机颜色
+const randomColor = () => "#" + Math.floor(Math.random() * 0xffffff).toString(16).padEnd(6, "0");
+
+// 随机字符串
+const randomString = () => Math.random().toString(36).slice(2);
+
+// 去掉字符串中的元素标记
+const removeTag = fragment => new DOMParser().parseFromString(fragment, "text/html").body.textContent || "";
