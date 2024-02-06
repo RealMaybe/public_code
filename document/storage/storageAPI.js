3a3f0a88aaa23d31f55f012c76e0a87258cda2c6
RealMaybe <realmaybe0429@qq.com>
2024-02-06 18:34:08 +0800

    less

--- document/storage/storageAPI.js
+++ document/storage/storageAPI.js
@@ -20,19 +20,25 @@ function StorageAPI(storageType) {
             throw new Error("Unknown environment, unable to determine storage method.");
     })()
 
+    /* ========== */
+
+    // 公共方法
+    function removeItem(key) { key ? storage.removeItem(key) : storage.clear() }
+
+    /* ========== */
 
     /**
      * 设置或获取存储数据
-     * @function StorageData
+     * @function Storage
      * @param { string } key - 数据的键名
      * @param { any } value - 要存储的值 (可选)
      * @returns { void | any } - 如果提供了值，则设置键的值；如果没有提供值，则返回键的存储值
      */
-    this.StorageData = function(key, value) {
+    this.Storage = function(key, value) {
         if (key && !value) {
             let storedValue = storage.getItem(key);
             return JSON.parse(storedValue);
-        } else if (value !== undefined) {
+        } else if (key && value !== undefined) {
             storage.setItem(key, JSON.stringify(value));
         } else {
             let storedValue = storage.getItem(key);
@@ -40,14 +46,33 @@ function StorageAPI(storageType) {
         }
     }
 
+
     /**
      * 删除存储数据
-     * @function DeleteStorageData
+     * @function Delete
      * @param { string } key - 要删除的数据的键名
      * @returns { void }
      */
-    this.DeleteStorageData = function(key) {
-        return storage.removeItem(key)
+    this.Delete = function(key) {
+        try {
+            removeItem(key)
+        } catch (error) {
+            console.error("Error deleting item from storage:", error)
+        }
+    }
+
+
+    /**
+     * 删除存储的所有数据
+     * @function Clean
+     * @returns { void }
+     */
+    this.Clean = function() {
+        try {
+            removeItem()
+        } catch (error) {
+            console.error("Error cleaning storage:", error)
+        }
     }
 }
 
